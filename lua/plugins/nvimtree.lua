1ab660f70fd0dc5b478e9986faf285e661b8288a
Mark <mark.n@nsysgroup.com>
2024-10-31 15:52:38 +0300

    fixed keybindings add ui

--- lua/plugins/nvimtree.lua
+++ lua/plugins/nvimtree.lua
@@ -4,10 +4,12 @@ return {
   lazy = false,
   dependencies = {
     "nvim-tree/nvim-web-devicons",
+    "b0o/nvim-tree-preview.lua",
   },
+  opts = {silent=true},
   config = function()
-    require("nvim-tree").setup {}
-    vim.keymap.set('n', '<C-n>', ":NvimTreeToggle<CR>", {})
-    vim.keymap.set('n', '<leader>e', ":NvimTreeFocus<CR>", {})
+    require("nvim-tree").setup(opts)
+    vim.keymap.set('n', '\\', ":NvimTreeToggle<CR>", {})
+    vim.keymap.set('n', '<leader>\\', ":NvimTreeFocus<CR>", {})
   end,
 }
